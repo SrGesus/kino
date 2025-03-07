0cc5b598f08be90dd743733a8ca2e9baa0422d06
SrGesus <108523575+SrGesus@users.noreply.github.com>
2025-03-07 22:27:36 +0000

    Add routes to file

--- apply.sh
+++ apply.sh
@@ -1,4 +1,4 @@
-
+#!/bin/bash -x
 # Apply Media Stack Plan 
 cd terraform-media-stack
 terraform init
--- nginx.tf
+++ nginx.tf
@@ -1,10 +1,15 @@
 locals {
   nginx_html   = "${abspath(path.module)}/html"
   nginx_config = "${abspath(path.module)}/nginx"
+  media_routes = "${abspath(path.module)}/nginx"
 }
 
-resource "local_file" "applications_nginx_confs" {
-  for_each = module.deployment.routes
+resource "local_file" "media_routes" {
+  filename = "${abspath(path.module)}/terraform-media-stack/routes.json"
+}
+
+resource "local_file" "applications_nginx_media_routes" {
+  for_each = jsondecode(local_file.media_routes)
   filename = "${local.nginx_config}/routes/${each.key}.conf"
   content  = <<-EOT
     %{if each.value.stripprefix == true}location /${each.key} {
@@ -37,7 +42,7 @@ resource "local_file" "html_website_list" {
   filename = "html/assets/applist.json"
   content = jsonencode(
     [
-      for key, value in module.deployment.routes : key
+      for key, value in jsondecode(local_file.media_routes) : key
     ]
   )
 }
